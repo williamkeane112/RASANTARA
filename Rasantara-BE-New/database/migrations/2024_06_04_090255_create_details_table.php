224d457980873bbfac7a12e4aff1b4ccdf482a77
williamkeane112 <williamkeanekeane@gmail.com>
2024-06-04 21:05:10 +0700

    add data

---
+++ Rasantara-BE-New/database/migrations/2024_06_04_090255_create_details_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('makanan_id')->constrained();
+            $table->text('tutorial');
+            $table->text('latarBelakang');
+            $table->text('bahanBahan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('details');
+    }
+};
