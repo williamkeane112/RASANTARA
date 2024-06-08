1f24d663475a661948d0550d8a0108d925c21304
Kaoei <fahmiirfarizal@gmail.com>
2024-06-08 17:39:01 +0700

    updt

--- src/Page/List.jsx
+++ src/Page/List.jsx
@@ -1,11 +1,20 @@
 import HideMenu from "../assets/icon/HideMenu.svg";
 import bgWalpp from "../assets/img/bgwater2.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot, faXmark, faSearch, faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot, faXmark, faSearch } from "@fortawesome/free-solid-svg-icons";
 import sumatera2Img from "../assets/img/SumateraImg2.png";
 import JawaImg2 from "../assets/img/JawaImg2.png";
+import ntbImg from "../assets/img/ntbImg.png";
+import nttImg from "../assets/img/nttImg.png";
+import KalimantanImg2 from "../assets/img/KalimantanImg2.png";
+import PapuaImg2 from "../assets/img/PapuaImg2.png";
+import SulawesiImg2 from "../assets/img/SulawesiImg2.png";
+import BaliImg2 from "../assets/img/BaliImg2.png";
+
 import ListComp from "../components/ListComp";
 import Sidebar from "../components/Sidebar";
+import MusikList from "../components/musikList";
+
 import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
 
@@ -22,17 +31,50 @@ function List() {
   const pulau = queryParams.get("pulau");
 
   let imageToShow;
+  let pulauName;
+
   switch (pulau) {
     case "sumatera":
       imageToShow = sumatera2Img;
+      pulauName = "SUMATERA";
       break;
     case "jawa":
-      imageToShow = JawaImg2; // corrected variable name
+      imageToShow = JawaImg2;
+      pulauName = "JAWA";
+      break;
+    case "ntt":
+      imageToShow = nttImg;
+      pulauName = "NUSA TENGGARA TIMUR";
+      break;
+    case "ntb":
+      imageToShow = ntbImg;
+      pulauName = "NUSA TENGGARA BARAT";
+      break;
+    case "kalimantan":
+      imageToShow = KalimantanImg2;
+      pulauName = "KALIMANTAN";
+      break;
+    case "papua":
+      imageToShow = PapuaImg2;
+      pulauName = "PAPUA";
+      break;
+    case "sulawesi":
+      imageToShow = SulawesiImg2;
+      pulauName = "SULAWESI";
+      break;
+    case "bali":
+      imageToShow = BaliImg2;
+      pulauName = "BALI";
       break;
     default:
       imageToShow = null;
+      pulauName = "";
       break;
   }
+
+  const isJawa = pulau === "jawa";
+  const isNttOrNtb = pulau === "ntt" || pulau === "ntb";
+  const isSumatera = pulau === "sumatera";
 
   return (
     <>
@@ -48,7 +90,7 @@ function List() {
               <FontAwesomeIcon icon={faXmark} className="text-2xl" />
             </Link>
           </div>
-          <div className="my-4 lg:mx-1 ml-8 mx-5 ">
+          <div className="my-4 lg:mx-1 ml-8 mx-5">
             <div className="flex items-center relative">
               <FontAwesomeIcon icon={faSearch} alt="Search icon" className="absolute w-4 mx-2" />
               <input type="text" placeholder={`Cari resep makan di daerah pulau ${location.pathname.replace("/", "")}`} className="w-full lg:text-md font-semibold text-sm pl-8 py-[5px] border border-black rounded-md" />
@@ -58,7 +100,7 @@ function List() {
         </section>
         <div className={`${Hide ? "lg:-translate-x-[542px] duration-1000" : "duration-1000"} absolute inset-0 flex justify-center items-center z-50 pointer-events-none`}>
           <div className="border bg-[#FCFCFC] border-black border-r-0 lg:rounded-l-2xl rounded-l-3xl lg:-ml-[30px] ml-auto pointer-events-auto">
-            <button className="lg:px-[12px] p-5  px-4 py-[6px]" onClick={HideOn}>
+            <button className="lg:px-[12px] p-5 px-4 py-[6px]" onClick={HideOn}>
               <img src={HideMenu} alt="Hide Menu" className={`lg:w-[8px] w-3 ${Hide ? "rotate-180 duration-700 ease-in-out " : "rotate-0 duration-700 ease-in-out"}`} />
             </button>
           </div>
@@ -67,12 +109,18 @@ function List() {
           Hide ? "lg:col-span-11 col-span-10 lg:-ml-12 translate-x-10 -ml-1 lg:w-[100.7%] w-[149%] transition-transform duration-1000 ease-in-out" : "hidden lg:w-full  duration-1000 ease-in-out"
         }`}>
           <div className="w-full h-screen" style={{ backgroundImage: `url(${bgWalpp})` }}>
-            <div className="flex items-center lg:pl-7 pl-2 pt-3">
-              <FontAwesomeIcon icon={faCircleExclamation} alt="" className="text-lg text-[#3D8E93] mx-2" />
-              <span className="lg:text-[11px] text-[14px] lg:mt-0 mt-5 font-semibold text-[#055F68]">Pilih Provinsi untuk menemukan resep makanan khas provinsi tersebut </span>
+            <div className="flex items-center justify-center lg:pl-7 pl-2 pt-3">
+              <span className="text-4xl font-bold text-center">{pulauName}</span>
             </div>
             <div className="flex items-center justify-center lg:mt-14 mt-44">
-              <img src={imageToShow} alt="Pulau Image" className="lg:w-[550px] w-[600px] mx-auto" />
+              <img
+                src={imageToShow}
+                alt="Pulau Image"
+                className={`mx-auto ${isSumatera ? "lg:w-[80vh]" : "lg:w-[100vh]"} w-[60vh] ${isJawa ? "mt-36" : ""} ${isNttOrNtb ? "mt-32" : ""}`}
+              />
+            </div>
+            <div className={`ml-5 ${isJawa ? "mt-28" : ""}`}>
+              <MusikList />
             </div>
           </div>
         </section>
---
+++ src/components/musikList.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeHigh, faVolumeMute } from "@fortawesome/free-solid-svg-icons";
+import manukDadali from "../assets/audio/manukdadali.m4a"; 
+import ayamdehlapeh from "../assets/audio/ayamdehlapeh.m4a";
+import sajojo from "../assets/audio/sajojo.m4a";
+import Yamko from "../assets/audio/Yamko.m4a";
+import { useLocation } from "react-router-dom";
+
+const musicList = [
+  { title: "Manuk Dadali", artist: "Lagu Tradisional Jawa Barat", audioSrc: manukDadali, region: "jawa" },
+  { title: "Ayam Deh Lapeh", artist: "Lagu Tradisional Sumatra", audioSrc: ayamdehlapeh, region: "sumatera" },
+  { title: "Sajojo", artist: "Lagu Papua", audioSrc: sajojo, region: "papua" },
+  { title: "Yamko Rambe Yamko", artist: "Lagu Papua", audioSrc: Yamko, region: "papua" },
+];
+
+function MusikList() {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const pulau = queryParams.get("pulau");
+
+  const filteredMusicList = musicList.filter(music => music.region === pulau);
+  const initialMusicIndex = Math.floor(Math.random() * filteredMusicList.length);
+
+  const [isMusicPlaying, setIsMusicPlaying] = useState(true);
+  const [currentMusicIndex, setCurrentMusicIndex] = useState(initialMusicIndex);
+  const [audio, setAudio] = useState(() => new Audio(filteredMusicList[initialMusicIndex].audioSrc));
+
+  useEffect(() => {
+    const handleEnded = () => {
+      const nextIndex = Math.floor(Math.random() * filteredMusicList.length);
+      setCurrentMusicIndex(nextIndex);
+      setAudio(new Audio(filteredMusicList[nextIndex].audioSrc));
+    };
+
+    audio.addEventListener("ended", handleEnded);
+
+    if (isMusicPlaying) {
+      audio.play();
+    }
+
+    return () => {
+      audio.pause();
+      audio.removeEventListener("ended", handleEnded);
+    };
+  }, [audio, isMusicPlaying]);
+
+  useEffect(() => {
+    if (isMusicPlaying) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }, [audio, isMusicPlaying]);
+
+  const handleAudioToggle = () => {
+    setIsMusicPlaying(!isMusicPlaying);
+  };
+
+  return (
+    <div className="">
+      <div className="music flex items-center md:gap-5 gap-2 text-[#2F7377]">
+        <button onClick={handleAudioToggle}>
+          <FontAwesomeIcon icon={isMusicPlaying ? faVolumeHigh : faVolumeMute} className="md:text-3xl text-xl" />
+        </button>
+        <div className="text">
+          <h1 className="music-title font-bold md:text-lg text-sm">
+            {filteredMusicList[currentMusicIndex].title}
+          </h1>
+          <p className="music-artist text-[10px] md:text-xs">
+            {filteredMusicList[currentMusicIndex].artist}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MusikList;
