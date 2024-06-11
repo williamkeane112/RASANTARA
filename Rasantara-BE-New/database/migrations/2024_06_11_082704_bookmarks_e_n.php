ae7b2792386bed8a8d66e91563c208df47d34aa5
Kaoei <fahmiirfarizal@gmail.com>
2024-06-11 15:29:35 +0700

    add model & migration

---
+++ Rasantara-BE-New/database/migrations/2024_06_11_082704_bookmarks_e_n.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookmarksEN', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('makanan_id')->constrained('makanans');
+            $table->enum('status', ['save', 'unsave'])->default('save');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookmarksEN');
+    }
+};
